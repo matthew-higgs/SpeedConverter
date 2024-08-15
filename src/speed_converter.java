9069c70d7624b9414d6c9c5b5d6f2bad31fe8a7d
Chewbacca <higgimat000@warren.k12.in.us>
2024-08-15 12:59:38 -0400

    initial commit

---
+++ src/speed_converter.java
@@ -0,0 +1,18 @@
+public class speed_converter {
+    public static void main(String[] args) {
+        System.out.println(toMilesPerHour(1.5));
+        System.out.println(toMilesPerHour(10.25));
+        System.out.println(toMilesPerHour(-5.6));
+        System.out.println(toMilesPerHour(25.42));
+        System.out.println(toMilesPerHour(75.114));
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour) {
+        if (kilometersPerHour < 0) {
+            return -1;
+        } else {
+            double milesPerHour = kilometersPerHour / 1.609;
+            return Math.round(milesPerHour);
+        }
+    }
+}
